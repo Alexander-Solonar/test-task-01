af504991b730a9fa00acb9e02cabbd471bedf23c
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-05-20 13:20:28 +0300

    completed hero

--- src/js/slide-section-hero.js
+++ src/js/slide-section-hero.js
@@ -1,4 +1,31 @@
+import sliderHero from '../data/sliderHero';
 import Swiper from '/node_modules/swiper/swiper-bundle.min';
+
+const heroSwiperWrapper = document.querySelector('.hero__swiper-wrapper');
+
+heroSwiperWrapper.innerHTML = sliderHero.reduce(
+  (html, { like, description, image }) => {
+    console.log(image);
+    return (
+      html +
+      `<div class="hero__swiper-slide swiper-slide">
+         <div class="box-inform">
+            <img
+               class="box-inform__user-avatar"
+                src="${image}"
+                alt="user-avatar"
+                width="54"
+             />
+          <div>
+               <h3 class="box-inform__fitback">${like}</h3>
+               <p class="box-inform__text">${description}</p>
+          </div>
+        </div>
+      </div>`
+    );
+  },
+  ''
+);
 
 new Swiper('.hero__swiper', {
   loop: true,
